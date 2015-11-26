105f7cd5314eebe50664c6cc907fb91fbb16810b
duckmin <allthingsrobert@gmail.com>
2015-11-26 00:29:14 -0500

    fixed post preview manager view

--- server/api/manager/template/get_post_preview_html.php
+++ server/api/manager/template/get_post_preview_html.php
@@ -4,6 +4,7 @@
 	$form_data = $json["post_data"];
 	$template_data = $json["template_data"];
 	$post_template = file_get_contents( TEMPLATE_DIR."/blog_post.txt" );
+	$post_preview = file_get_contents( TEMPLATE_DIR."/post_preview.txt" );		
 	
 	$single = array();
 	$single["_id"] ="5428784f7f8b9afe1a779e93";  //just a dummy ID means nothing 
@@ -13,6 +14,24 @@
 	$single["author"] = $_SESSION['user'];
 	$single["description"] = $form_data["description"];
 	$single["hashtags"] = $post_view->extractHashtagsFromPostData( $template_data );
+	$single["preview_text"] = $post_view->getPreviewTextFromMarkdown( $template_data );
 	
 	echo $post_view->makePostHtmlFromData( $single, $post_template );
+	
+	//for preview only want post_data array populated with one 'data-posttype':'image' item
+	$single["post_data"] = array();
+	//return the first one in an array
+	foreach( $template_data as $item ){
+	    if( $item["data-posttype"] === "image" ){
+	        $single["post_data"] = array($item);
+	        break;
+	    }
+	}
+
+	//if image not found unset and preview method logic kicks in
+	if( count($single["post_data"]) !== 1 ){
+	    unset($single["post_data"]);
+	} 
+	
+	echo $post_view->makePostPreviewHtmlFromData( $single, $post_preview );
 ?>
